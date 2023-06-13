06c99b42fc909a9100c220e6de6cb9d867016c4a
bob <ilya.kvasov.02@list.ru>
2023-06-13 21:16:47 +0300

    валидация

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,17 +1,44 @@
-import { Controller, Get, Post, Req, Res, Body } from '@nestjs/common';
-import { Request, Response } from 'express';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Query,
+  UsePipes,
+  ValidationPipe,
+  ParseIntPipe,
+  ParseBoolPipe,
+} from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 
 @Controller('users')
 export class UsersController {
   @Get()
-  getUsers() {
+  getUsers(@Query('sortDesc', ParseBoolPipe) sortDesc: boolean) {
+    // Query запрос обрабатывается декоратором Query из @nestjs/common
+    console.log(sortDesc);
     return { userName: 'Inson', email: 'inson.com' };
   }
 
-  @Post('')
+  // Все POST запросы с валидацией из пакета class-validator
+  @Post('create')
+  @UsePipes(new ValidationPipe()) //подключаю валидацию, которую описал в DTO
   createUser(@Body() userData: CreateUserDto) {
+    // POST запрос, при котором летят данные нужно обрабатывать декоратором
+    // @Body из @nestjs/common с использованием DTO для ускоренной обработки данных
     console.log(userData);
     return {};
   }
+
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: string) {
+    //ParseIntPipe = numeric string is expected
+    // Обрабротка запроса с дополнительными параметрами происходит с декоратором
+    // @Param из @nestjs/common
+    console.log(id);
+    return { id };
+  }
+
+  //остальные запросы DELETE PUT PATCH так же подятгиваются из @nestjs/common
 }
